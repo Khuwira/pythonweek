ca4de6ba1346c516b54d64028ef7e265dc3346a5
Khuwira <69214376+Khuwira@users.noreply.github.com>
2021-10-21 13:17:31 +0200

    Add files via upload

---
+++ area.py
@@ -0,0 +1,15 @@
+""" functions that compute areas of shapes """
+def square(a):
+    """ calculates square """
+    print(a*a)
+
+def circle(r):
+    """ calculates circle """
+    from math import pi
+    print(pi*r*r)
+    
+def triangle(b,h):
+    """ calculates """
+    return(0.5*b*h)
+    
+    
---
+++ greetings.py
@@ -0,0 +1,7 @@
+def mwadzuka(name):
+    """" morning greetingvin chichewa lang! """
+    print("mwazuka bwanji " + name)
+def mwatandala(name):
+    print("mwatandala " + name )
+mwadzuka('frank')
+mwatandala('frank')
